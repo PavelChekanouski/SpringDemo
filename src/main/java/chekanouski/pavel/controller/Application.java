a341a2a64198aa3f3984580387168cb1d7ef7030
Pavel Chekanouski <PavelChekanousli@gmail.com>
2020-09-22 12:57:56 +0300

    Fix: some small fixes of unused code

--- src/main/java/chekanouski/pavel/controller/Application.java
+++ src/main/java/chekanouski/pavel/controller/Application.java
@@ -1,10 +1,8 @@
 package chekanouski.pavel.controller;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import java.util.Collections;
@@ -12,7 +10,7 @@ import java.util.Collections;
 @SpringBootApplication
 @EntityScan("chekanouski.pavel.entity")
 @EnableJpaRepositories("chekanouski.pavel.repository")
-//@ComponentScan("chekanouski.pavel.service")
+
 public class Application {
 
     public static void main(String[] args) {
--- src/main/java/chekanouski/pavel/controller/ProductController.java
+++ src/main/java/chekanouski/pavel/controller/ProductController.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 
