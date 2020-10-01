b4f9a0f9548eaf11db66c6ede6089e64744d330a
Pavel Chekanouski <PavelChekanousli@gmail.com>
2020-10-01 14:02:25 +0300

    Refactor: added new table Brands. Connected to Products OneToOne.

--- src/main/java/chekanouski/pavel/entity/Brand.java
+++ src/main/java/chekanouski/pavel/entity/Brand.java
@@ -1,33 +1,30 @@
 package chekanouski.pavel.entity;
 
-import lombok.*;
+import lombok.Getter;
+import lombok.Setter;
 
 import javax.persistence.*;
 import java.util.Objects;
 
 @Entity
-@Table(name = "products")
-@Getter @Setter
-public class Product {
+@Table(name = "brands")
+@Getter
+@Setter
+public class Brand {
+
     @Id
     @GeneratedValue(strategy= GenerationType.AUTO)
     @Column(name = "id", unique = true, nullable = false, updatable = false)
     private int id;
-
     @Column(name = "name", nullable = false, updatable = false)
     private String name;
-
-    @OneToOne(cascade = CascadeType.ALL)
-    @JoinColumn(name= "brand_id", referencedColumnName = "id")
-    public Brand brand;
-
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-        Product product = (Product) o;
-        return Objects.equals(name, product.name);
+        Brand brand = (Brand) o;
+        return Objects.equals(name, brand.name);
     }
 
     @Override
